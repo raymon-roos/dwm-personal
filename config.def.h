4e299df69af4862336819deced031da8f3f3edba
Raymon Roos <raymon.roos@student.rocva.nl>
2021-11-29 17:11:46 +0100

    attempted to program in a keybind for librewolf

--- config.def.h
+++ config.def.h
@@ -61,10 +61,12 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *slock[] = { "slock" , NULL };
+static const char *web[] = { "librewolf, NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,			XK_b,	   spawn,	   {.v = web } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
