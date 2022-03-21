15cc0174fc2e511cc9806874f10d83876271d764
Raymon Roos <raymon.roos@student.rocva.nl>
2022-03-21 22:05:54 +0100

    Updated config.def.h
    - Added hotkeys for launching sfm en top as terminal commands

--- config.def.h
+++ config.def.h
@@ -65,6 +65,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *filebrws[]  = { "st", "-e", "sfm", NULL };
+static const char *top[]  = { "st", "-e", "top", NULL };
 static const char *slock[] = { "slock", NULL };
 static const char *web[] = { "librewolf", NULL };
 
@@ -72,6 +74,8 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,			XK_b,	   spawn,	   {.v = web } },
+	{ MODKEY|ShiftMask,		XK_t,	   spawn,	   {.v = top } },
+	{ MODKEY,			XK_f,	   spawn,	   {.v = filebrws } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,		XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -92,7 +96,7 @@ static Key keys[] = {
 	{ MODKEY|ControlMask|ShiftMask,	XK_l,	   spawn,	   {.v = slock } },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },/*Tiled*/
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },/*Floating*/
+	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },/*Floating*/
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },/*Monocle*/
 	{ MODKEY|ShiftMask,		XK_m,	   setlayout,	   {.v = &layouts[3]} },/*Center master tiled*/
 	{ MODKEY|ControlMask|ShiftMask,	XK_m,	   setlayout,	   {.v = &layouts[4]} },/*Center master float*/
