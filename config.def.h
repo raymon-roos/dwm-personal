ae9c7dee3071611180493e45ffe52eab42d200c9
Raymon Roos <raymon.roos@hotmail.com>
2022-06-11 00:14:22 +0200

    Committing current changes before floatrules patch
    Added "vanilla" rules for Thunderbird Msgcomposition
    windows.
    
    Changed font settings.
    
    Changed selected titlebar background color,
    to more easily see which monitor is in focus.
    
    Reduced snap pixels for smooth moving/resizing

--- config.def.h
+++ config.def.h
@@ -2,11 +2,11 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 16;       /* snap pixel */
+static const unsigned int snap      = 1;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Source Code Pro:pixelsize=11:antialias=true" };
+static const char dmenufont[]       = "Source Code Pro:pixelsize=11:antialias=true";
 static const char col_gray0[]       = "#F4F6F6";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -17,7 +17,7 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_cyan },
 	[SchemeSel]  = { col_gray4, col_cyan, col_gray0  },
-	[SchemeTitle]  = { col_gray0, col_gray1,  col_cyan  },
+	[SchemeTitle]  = { col_gray0, col_gray2,  col_cyan  },
 };
 
 /* tagging */
@@ -30,7 +30,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Librewolf",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Thunderbird", "Msgcompose", NULL,  0,            1,           -1 },
 };
 
 /* layout(s) */
