52d2f10e3ac9cbd30f37874164db40d9ffccae78
Raymon Roos <raymon.roos@hotmail.com>
2022-06-11 01:37:01 +0200

    Applied centered patch
    To make clients matching floatrules spawn
    in the center of current monitor.

--- config.def.h
+++ config.def.h
@@ -28,9 +28,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Thunderbird", "Msgcompose", NULL,  0,            1,           -1 },
+	/* class 	 instance     title  tags mask  iscentered  isfloating  monitor */
+	{ "Thunderbird", "Msgcompose", NULL, 0,         1,          1,          -1 },
+	{ "Thunderbird", "Calendar",   NULL, 0,         1,          1,          -1 },
 };
 
 /* layout(s) */
