db42c99ae88c38541d4f321629f5f900009e0612
Raymon Roos <raymon.roos@hotmail.com>
2023-02-12 13:13:26 +0100

    fix: get rid of unused connman keybind

--- config.def.h
+++ config.def.h
@@ -67,7 +67,6 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *con[]  = { "st", "-e", "connmanctl", NULL };
 static const char *slock[] = { "slock", NULL };
 static const char *web[] = { "librewolf", NULL };
 static const char *guimail[] = { "thunderbird", NULL };
@@ -76,7 +75,6 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,			XK_b,	   spawn,	   {.v = web } },
-	{ MODKEY,			XK_c,	   spawn,	   {.v = con } },
 	{ MODKEY|ShiftMask,             XK_t,      spawn,          TRMCMD("top") },
 	{ MODKEY,                       XK_f,      spawn,          TRMCMD("launch_nnn.sh") },
 	{ MODKEY,                       XK_n,      spawn,          {.v = guimail } },
